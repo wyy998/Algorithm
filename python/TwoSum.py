46b4f1fe2a46d3b1d75a3c38eb0de5028b4a7b1d
wyy998 <1490473564@qq.com>
2021-03-24 21:03:31 +0800

    init python with two sum

---
+++ python/TwoSum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            d[nums[i]] = i
+        for i in range(len(nums)):
+            if target - nums[i] in d and d[target - nums[i]] != i:
+                return [i, d[target - nums[i]]]
+        return []
+
+
+s = Solution()
+assert sorted(s.twoSum([2, 7, 11, 15], 9), reverse=False) == [0, 1]
+assert sorted(s.twoSum([3, 2, 4], 6), reverse=False) == [1, 2]
